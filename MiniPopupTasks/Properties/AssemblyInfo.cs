430d7f7c4fac3bd22ac7553879b1a45713c16a7e
francois <francois@2e8daa4d-41f8-c944-804a-01035b6eed2e>
2012-09-10 18:09:54 +0000

    git-svn-id: svn://localhost/minipopuptasks/trunk@6 2e8daa4d-41f8-c944-804a-01035b6eed2e

--- MiniPopupTasks/Properties/AssemblyInfo.cs
+++ MiniPopupTasks/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WpfApplication1")]
+[assembly: AssemblyTitle("MiniPopupTasks")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("WpfApplication1")]
+[assembly: AssemblyProduct("MiniPopupTasks")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -52,4 +52,4 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.19")]
+[assembly: AssemblyFileVersion("1.0.0.54")]
